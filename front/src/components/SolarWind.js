1efb06520952ab2fcb1c507359addded8b1481fd
Neta <netarvd@gmail.com>
2022-10-02 09:37:41 +0300

    cut the bullshit and created Rain component

--- front/src/components/SolarWind.js
+++ front/src/components/SolarWind.js
@@ -1,5 +1,24 @@
-import React, { useState } from 'react'
-import ReactRain from 'react-rain-animation'
+import React, { useState } from 'react';
+import ReactRain from 'react-rain-animation';
+import Rain from './Rain';
+
+// function randRange( minNum, maxNum) {
+//     return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+//   }
+  
+//   // function to generate drops
+//   function createRain() {
+  
+//       for( i=1;i<nbDrop;i++) {
+//       var dropLeft = randRange(0,1600);
+//       var dropTop = randRange(-1000,1400);
+  
+//       $('.rain').append('<div class="drop" id="drop'+i+'"></div>');
+//       $('#drop'+i).css('left',dropLeft);
+//       $('#drop'+i).css('top',dropTop);
+//       }
+  
+//   }
 
 const SOLAR_WIND_CLASSES = {
   A: 'green-300',
@@ -10,19 +29,18 @@ const SOLAR_WIND_CLASSES = {
 }
 
 const determineNumOfDrops = (degree) => {
-  return degree * 10
+  return degree * 10;
 }
 
 const SolarWind = ({ type }) => {
   const solarWindClass = type[0]
-  const degree = type.slice(1)
+  const degree = parseInt(type.slice(1))
 
   const [numOfDrops, setNumOfDrops] = useState(determineNumOfDrops(degree))
 
   return (
     <div className={`bg-black text-${SOLAR_WIND_CLASSES[solarWindClass]}`}>
-      <div className='text-pink-700'>fdsfas</div>
-      <ReactRain numOfDrops={numOfDrops} />
+        <Rain numDrops={numOfDrops} />
     </div>
   )
 }
