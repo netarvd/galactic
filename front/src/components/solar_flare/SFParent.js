ed8e5f01f8756ea70e9e37fb373a0069a272e09c
amitay <amitay1599@gmail.com>
2022-10-06 19:11:30 +0300

    name changes

--- front/src/components/solar_flare/SFParent.js
+++ front/src/components/solar_flare/SFParent.js
@@ -3,8 +3,12 @@ import React, { PureComponent, useState } from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import SFChild from './SFChild';
 
+const circle = (num, opac) => { 
+  return <div className={`rounded-full w-6 h-6 grid place-items-center bg-[#FF5604] text-[#FF5604] text-sm bg-opacity-[.${opac}] font-bold aspect-square`}>{num}</div>
+} 
 
 function Flr({flrData}) {
+
   if(flrData) { 
     return (
     <div className='p-4 text-lg bg-white bg-opacity-10 rounded-xl h-full col-span-4 xl:col-span-2'>
@@ -14,14 +18,14 @@ function Flr({flrData}) {
           <h1 className='text-lg font-light text-opacity-80 text-white'>Peak time classification</h1>
           </div>
           <div className=' grid items-end'>
-          <div className='content-end flex align-bottom gap-2 items-end text-[#FF5604] font-semibold'>
-            <h1 className='text-sm content-end bg-bottom items-end'>Danger bar</h1>
-            <div className='rounded-full px-2 bg-[#FF5604] text-[#FF5604] text-sm py-1 bg-opacity-5 text-opacity-50 font-bold'>A</div>
-            <div className='rounded-full px-2 bg-[#FF5604] text-[#FF5604] text-sm py-1 bg-opacity-25 font-bold text-opacity-60'>B</div>
-            <div className='px-2 text-[#FF5604] text-sm py-1 font-bold bg-[#FF5604] bg-opacity-50 rounded-full text-opacity-70'>C</div>
-            <div className='px-2 text-white text-opacity-60 text-sm py-1 font-bold bg-[#FF5604] bg-opacity-75 rounded-full'>M</div>
-            <div className='px-2 text-white text-opacity-90 text-sm py-1 font-bold bg-[#FF5604] bg-opacity-90 rounded-full '>X</div>
-          </div>
+          {/* <div className='content-end flex align-bottom gap-2 items-end text-[#FF5604] font-semibold'>
+            <h1 className='text-sm content-end bg-bottom items-end bg-opac'>Danger bar</h1>
+            {circle('A', '20')}
+            {circle('B', '30')}
+            {circle('C', '30')}
+            {circle('M', '30')}
+            {circle('X', '45')}
+          </div> */}
         </div>
       </div>
       <SFChild flrData={flrData}/>
